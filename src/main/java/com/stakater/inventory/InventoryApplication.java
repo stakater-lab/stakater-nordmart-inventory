205d5d12e5193e733ce4e714ac64ce33c1c812c6
AsfaMumtaz <asfamumtaz@gmail.com>
2022-03-15 16:34:55 +0500

    Update InventoryApplication.java

--- src/main/java/com/stakater/inventory/InventoryApplication.java
+++ src/main/java/com/stakater/inventory/InventoryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InventoryApplication {
 
-	breakpubligffgc static void main(String[] args) {
+	breakpfubligffgc static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
 
